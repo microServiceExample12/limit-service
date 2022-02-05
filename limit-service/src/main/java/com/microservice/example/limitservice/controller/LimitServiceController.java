81e42add6fd3f0a9cd7f5cc943cd016d6ca3458b
Dubey <rdubey26@dxc.com>
2022-02-05 17:43:26 +0530

    comiit

--- limit-service/src/main/java/com/microservice/example/limitservice/controller/LimitServiceController.java
+++ limit-service/src/main/java/com/microservice/example/limitservice/controller/LimitServiceController.java
@@ -1,17 +1,24 @@
 package com.microservice.example.limitservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.microservice.example.limitservice.configuration.Configuration;
 import com.microservice.example.limitservice.entity.Limits;
 
 @RestController
 public class LimitServiceController {
 	
+	@Autowired
+	private Configuration config;
+	
+	
 	@GetMapping("/limits")
 	public Limits getLimit()
 	{
-		return new Limits(1,1000);
+		return new Limits(config.getMinimum(), config.getMaximum());
+	//	return new Limits(5,1000);
 	}
 
 }
